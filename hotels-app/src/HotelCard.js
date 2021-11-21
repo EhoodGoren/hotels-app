a113e881f97a79b80ba0457332e620718f5c14e7
EhoodGoren <ehoodgoren@gmail.com>
2021-11-21 23:07:48 +0200

    Added more design, and images for every hotel card.

--- hotels-app/src/HotelCard.js
+++ hotels-app/src/HotelCard.js
@@ -5,10 +5,9 @@ export default class HotelCard extends React.Component {
     render() {
         return (
             <div className='hotel-card'>
-                <div className='hotel-img'>picture</div>
-                {/* <img className='hotel-img' alt='placeholder' src="https://www.lovemysalad.com/sites/default/files/styles/image_530x397/public/banaan-large.jpg?itok=dZxH6D_w"/> */}
-                <Link to={encodeURI(this.props.hotel.name)}>
-                    <div className='hotel-name'>{this.props.hotel.name}</div>
+                <img className='hotel-img' alt='hotel' src={this.props.hotel.img} />
+                <Link className='hotel-name' to={encodeURI(this.props.hotel.name)}>
+                    <div>{this.props.hotel.name}</div>
                 </Link>
             </div>
         )
