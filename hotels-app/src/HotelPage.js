8a64cf23d1bd148f295ed262cc24f5ff1ef311e6
EhoodGoren <ehoodgoren@gmail.com>
2021-11-21 23:41:55 +0200

    Added a 'go-back' button on every hotel page, to go back to the main page.

--- hotels-app/src/HotelPage.js
+++ hotels-app/src/HotelPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import CallNowButton from './CallNowButton';
 import './HotelPage.css';
+import { Link } from 'react-router-dom';
 
 export default class HotelPage extends React.Component {
     render() {
         return (
             <div className='hotel-page'>
+                <Link className='return-btn' to={'/'}>
+                    <div>⬅️</div>
+                </Link>
                 <div className='page-img'>picture</div>
-                {/* img */}
+                <img className='hotel-img' alt='hotel' src={this.props.hotel.img} />
                 <div className='hotel-info'>
                     <div>Hotel name: {this.props.hotel.name}</div>
                     <div>Address: {this.props.hotel.street}</div>
